2fa922748578dc018e0a58b5b745eeaef42290c4
SimoneLollini <lollinisimone@gmail.com>
2022-10-06 17:03:29 +0200

    fix calculations

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,21 +2,36 @@
 // Il programma dovrà chiedere all'utente:
 //  chiedere il numero di chilometri che vuole percorrere
 const kmPercorsi = prompt("Quanti km devi percorrere?")
-console.log(kmPercorsi);
+
 
 // chiedere l'età del passeggero.
 const passengerAge = prompt("Quanti anni hai?")
-console.log(passengerAge);
+
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const price = kmPercorsi * 0.21
-console.log(price);
+const price = (kmPercorsi * 0.21)
+
 // va applicato uno sconto del 20% per i minorenni
+const discountUnder = (price * 0.20);
+
+// va applicato uno sconto del 40% per gli over 65. 
+const discountOver = (price * 0.40);
+
 if (passengerAge < 18) {
-    console.log(price * 0.20);
+    const finalPriceUnder = price - discountUnder
+    console.log(finalPriceUnder.toFixed(2));
+    document.getElementById('ticket_price').innerHTML = (finalPriceUnder + " " + "€")
+
 } else if (passengerAge > 65) {
-    console.log(price * 0.40);
+    const finalPriceOver = price - discountOver
+    console.log(finalPriceOver.toFixed(2));
+    document.getElementById('ticket_price').innerHTML = (finalPriceOver + " " + "€")
+
+} else {
+    console.log(price.toFixed(2));
+    document.getElementById('ticket_price').innerHTML = (price + " " + "€")
 }
-// va applicato uno sconto del 40% per gli over 65.
+
+
 // L'output del prezzo finale va messo fuori in forma umana con massimo due decimali.
